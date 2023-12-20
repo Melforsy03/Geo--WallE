38239a2944fa7a330c3d35bbcf28e061f07eaac4
melani <forsythemelani@gmail.com>
2023-12-19 20:19:50 -0500

    actualizacion

--- ConexionPintarLogica.cs
+++ ConexionPintarLogica.cs
@@ -8,9 +8,9 @@ namespace ConexionPinturaLogica
     {
     public static void Main()
     {
-    //string input = ""
+  
     List<Errors> errors = new List<Errors>();
-    List<token> m = Tokenizar.TokenizeString("point p1 (6 , 50) ; \r\npoint p2(49,150);\r\npoint p3 (106 , 93) ;", errors);
+    List<token> m = Tokenizar.TokenizeString(Console.ReadLine(), errors);
     for (int i = 0; i < m.Count; i++)
     {
         Console.WriteLine(m[i].Value + " - "+ m[i].Type);
